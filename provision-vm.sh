eb6d376f8d954932c3106f74d9086a9ade134cc8
Nicolai Brunvoll Ernst <nicolai@ernst.io>
2018-08-16 14:55:20 +0200

    updated provision-vm.sh file with new structure

--- provision-vm.sh
+++ provision-vm.sh
@@ -5,15 +5,43 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Root directory in the git repo is mounted inside the VM in
 VAGRANTDIR=/vagrant
 
-echo "Executing each install-script in files"
 
-cd ${VAGRANTDIR}/scripts/vm-install.d
-find . -type f -name '*.sh' | sort -n | while read SHELLSCRIPT
-  do
-	  echo "########## Running: ${SHELLSCRIPT} ##########"
-    bash ${SHELLSCRIPT} 2>&1 
-  done
+# Installing and configuring all the applications for running DDPS-demo
+echo "Executing each install.sh script in all the folders in files/"
+echo "#############################################################"
+
+echo "########## Installing: OS Patches ##########"
+  ${VAGRANTDIR}/os-patches/install.sh
+
+echo "########## Installing: PostgreSQL ##########"
+  ${VAGRANTDIR}/postgresql/install.sh
+
+echo "########## Installing: Pgpool-II ##########"
+  ${VAGRANTDIR}/pgpool-II/install.sh
+
+echo "########## Installing: NGINX ##########"
+  ${VAGRANTDIR}/nginx/install.sh
+
+echo "########## Installing: Node.js ##########"
+  ${VAGRANTDIR}/node/install.sh
+
+echo "########## Installing: ExaBGP ##########"
+  ${VAGRANTDIR}/exabgp/install.sh
+
+echo "########## Installing: db2dps ##########"
+  ${VAGRANTDIR}/db2dps/install.sh
+
+echo "########## Installing: API-app ##########"
+  ${VAGRANTDIR}/api-app/install.sh
+
+echo "########## Installing: WEB-app ##########"
+  ${VAGRANTDIR}/web-app/install.sh
+
+echo "#####################"
+echo "Installation complete"
+echo "#####################"
 
 exit 0
