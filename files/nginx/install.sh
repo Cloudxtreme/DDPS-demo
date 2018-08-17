3e24dcfe0c0f4083c7f1627db1d452a0080be4c1
Nicolai Brunvoll Ernst <nicolai@ernst.io>
2018-08-17 11:04:03 +0200

    call /bin/bash in provision-vm.sh, so install.sh script doesn't have to have +x

--- files/nginx/install.sh
+++ files/nginx/install.sh
@@ -2,5 +2,6 @@
 
 echo "Installing NGINX ..."
 echo "Make sure that NGINX listen on 0.0.0.0 port 8080 and 9090!"
-echo "The NGINX must redirect traffic coming in on port 8080 and 9090 to the Node apps on localhost."
-echo "If the Node API-app listen on 127.0.0.1:9999 NGINX must redirect port 9090 to 127.0.0.1:9999"
+echo "NGINX must redirect traffic on port 8080 and 9090 to the Node-apps on localhost."
+echo "E.g. if the Node API-app listen on 127.0.0.1:9999"
+echo "NGINX must redirect traffic incomming on 0.0.0.0:9090 to 127.0.0.1:9999"
--- provision-vm.sh
+++ provision-vm.sh
@@ -17,31 +17,31 @@ echo "# Executing each install.sh script for each application #"
 echo "#########################################################"
 echo
 echo "########## Installing: OS Patches ##########"
-  ${VAGRANTDIR}/files/os-patches/install.sh
+  /bin/bash ${VAGRANTDIR}/files/os-patches/install.sh
 echo
 echo "########## Installing: PostgreSQL ##########"
-  ${VAGRANTDIR}/files/postgresql/install.sh
+  /bin/bash ${VAGRANTDIR}/files/postgresql/install.sh
 echo
 echo "########## Installing: Pgpool-II ##########"
-  ${VAGRANTDIR}/files/pgpool-II/install.sh
+  /bin/bash ${VAGRANTDIR}/files/pgpool-II/install.sh
 echo
 echo "########## Installing: NGINX ##########"
-  ${VAGRANTDIR}/files/nginx/install.sh
+  /bin/bash ${VAGRANTDIR}/files/nginx/install.sh
 echo
 echo "########## Installing: Node.js ##########"
-  ${VAGRANTDIR}/files/node/install.sh
+  /bin/bash ${VAGRANTDIR}/files/node/install.sh
 echo
 echo "########## Installing: ExaBGP ##########"
-  ${VAGRANTDIR}/files/exabgp/install.sh
+  /bin/bash ${VAGRANTDIR}/files/exabgp/install.sh
 echo
 echo "########## Installing: db2dps ##########"
-  ${VAGRANTDIR}/files/db2dps/install.sh
+  /bin/bash ${VAGRANTDIR}/files/db2dps/install.sh
 echo
 echo "########## Installing: API-app ##########"
-  ${VAGRANTDIR}/files/api-app/install.sh
+  /bin/bash ${VAGRANTDIR}/files/api-app/install.sh
 echo
 echo "########## Installing: WEB-app ##########"
-  ${VAGRANTDIR}/files/web-app/install.sh
+  /bin/bash ${VAGRANTDIR}/files/web-app/install.sh
 echo
 echo "#################################################################"
 echo "# Installation complete: Run 'vagrant reload' to reboot VM now! #"
