f2e0aa3bd41607824cdba8bc2b5c24c271c194cd
Nicolai Brunvoll Ernst <nicolai@ernst.io>
2018-08-20 15:17:10 +0200

    moved checks to each application

--- files/vagrant-report/check-services.sh
+++ files/vagrant-report/check-services.sh
@@ -2,20 +2,28 @@
 
 # This script checks various services and reports back to user during provision.
 
-## Check services
-echo "Check PostgreSQL configuration OK"
-echo "---------------------------------"
+# Check for running services
+
+## PostgreSQL
+echo "----------------------"
+echo "Is PostgreSQL running?"
+/usr/sbin/service postgresql status |grep Active:
+echo
+echo "Is PostgreSQL configuration OK?"
 HBA_FILE=`sudo su postgres -c "psql -t -P format=unaligned -c 'show hba_file';"`
 if [ -f $HBA_FILE ]; then
-    echo "psql running and reporting $HBA_FILE as config file ok"
+    echo "psql is running and reporting $HBA_FILE as config file: OK"
 else
-    echo "postgres installation failed"
+    echo "PostgresSQL configuration failed to load"
 fi
 echo
 
-#  check reading from database via pgpool2
+## Pgpool-II
+echo "---------------------"
+echo "Is pgpool-II running?"
+/usr/sbin/service pgpool2 status |grep Active:
+echo
 echo "Check connection to database via pgpool2"
-echo "----------------------------------------"
 export PGPASSWORD="password"
 Q="select pg_is_in_recovery();"
 Q="SELECT
@@ -25,39 +33,32 @@ FROM flow.fastnetmoninstances where vpn_ip_addr = '192.168.67.3';"
 LINES=`echo "${Q}" | psql -t -F' ' -h 127.0.0.1 -A -U flowuser -v ON_ERROR_STOP=1 -w -d netflow | wc -l`
 
 if [ $LINES -gt 100 ]; then
-    echo "read $LINES (expected 119) from database ok using pgpool2"
+    echo "Read $LINES (expected 119) from database ok using pgpool2"
 else
-    echo "failed reading from database using pgpool2"
+    echo "Failed reading from database using pgpool2"
 fi
 echo
 
-## Check for running services
-echo "----------------------"
-echo "Is PostgreSQL running?"
-/usr/sbin/service postgresql status |grep Active:
-echo
-
-echo "---------------------"
-echo "Is pgpool-II running?"
-/usr/sbin/service pgpool2 status |grep Active:
-echo
-
+## NGINX
 echo "-----------------"
 echo "Is NGINX running?"
 /usr/sbin/service nginx status |grep Active:
 echo
 
+## DDPS WEB-app
 echo "-----------------------"
 echo "Is the WEB-app running?"
 echo "I don't know..."
 echo
 
+## DDPS API-app
 echo "-----------------------"
 echo "Is the API-app running?"
 echo "I don't know..."
 echo
 
 
+# Check that network services are running correctly
 ## Check for IPv4 TCP services running on localhost
 echo "-----------------------------------------------"
 echo "TCP services listeing on localhost (127.0.0.1):"
