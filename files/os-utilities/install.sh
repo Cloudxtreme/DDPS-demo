06e328ec69a3882b11f6210418af0ea8137a0bc5
Niels Thomas Haugård <thomas@haugaard.net>
2018-08-20 09:25:11 +0200

    os-utils

--- files/os-utilities/install.sh
+++ files/os-utilities/install.sh
@@ -1,4 +1,54 @@
-#!/bin/bash
+#! /usr/bin/env bash
+#
+#   Copyright 2017, DeiC, Niels Thomas Haugård
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 
-echo "Installing OS utilities  ..."
-echo "Here we can install various other utilities we need."
+function generate_locales()
+{
+    locale-gen en_DK.utf8       # prevent perl from complaining during tests (Niels Thomas)
+    locale-gen da_DK.UTF-8      # danish locale required by postgres (sorting)
+}
+
+
+function main()
+{
+    # check on how to suppress newline (found in an Oracle installation script ca 1992)
+    echo="/bin/echo"
+    case ${N}$C in
+        "") if $echo "\c" | grep c >/dev/null 2>&1; then
+            N='-n'
+        else
+            C='\c'
+        fi ;;
+    esac
+
+    echo "Installing OS utilities  ..."
+    generate_locales
+
+    # your code goes here ...
+
+    exit 0
+
+}
+
+################################################################################
+# Main
+################################################################################
+#
+# clean up on trap(s)
+#
+
+main $*
+
