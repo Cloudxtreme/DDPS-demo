fa6760fa449e08ed158a577334106b8a5fe3c3df
Kasper Sort <kasper.sort@deic.dk>
2018-08-20 09:46:04 +0200

    Added pgpass installation

--- files/pgpool-II/configure.sh
+++ files/pgpool-II/configure.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-# #
+# Set date for backup files
 DATO=$(date +%Y%m%d%H%M%S)
 
 # Template directories
-TMPLPATH=$(pwd)
+TMPLPATH='/vagrant/files/pgpool-II'
 TMPLFILE='pgpool.conf.tmpl'
 TEMPDIR='/tmp'
 
@@ -13,32 +13,45 @@ CONFDIR='/etc/pgpool2'
 CONFFILE='pgpool.conf'
 CONFBAK='pgpool.conf.'$DATO
 
-#
 # pool_hba.conf
 TMPLHBAFILE='pool_hba.conf.tmpl'
-
 CONFHBAFILE='pool_hba.conf'
 CONFHBABAK='pool_hba.conf.'$DATO
 
 ( 
-	. env.sh
+	. /vagrant/files/pgpool-II/env.sh
 	envsubst < $TMPLPATH/$TMPLFILE > $TEMPDIR/$CONFFILE
 	envsubst < $TMPLPATH/$TMPLHBAFILE > $TEMPDIR/$CONFHBAFILE
 )
 
-# Copy files
+# Backup config files
 cp $CONFDIR/$CONFFILE $CONFDIR/$CONFBAK
-cp $TEMPDIR/$CONFFILE $CONFDIR/$CONFFILE
-
-# Backup file
 cp $CONFDIR/$CONFHBAFILE $CONFDIR/$CONFHBABAK
+
+# Copy config files
+cp $TEMPDIR/$CONFFILE $CONFDIR/$CONFFILE
 cp $TEMPDIR/$CONFHBAFILE $CONFDIR/$CONFHBAFILE
 
+
+# This should be created under postgresql installation
+# pgpass file
+PGPASSTMPL='pgpass.tmpl'
+PGPASSFILE='.pgpass'
+PGPATH='/var/lib/postgresql'
+ROOTPATH='/root'
+cp $TMPLPATH/$PGPASSTMPL $PGPASS/$PGPASSFILE
+cp $TMPLPATH/$PGPASSTMPL $ROOTPATH/$PGPASSFILE
+
+# Ensure ownership and permissions are correct
 chown postgres:postgres $CONFDIR/$CONFFILE
 chown postgres:postgres $CONFDIR/$CONFHBAFILE
+chown postgres:postgres $PGPASS/$PGPASSFILE
+chown root:root $ROOTPATH/$PGPASSFILE
 
 chmod 0640 $CONFDIR/$CONFFILE
 chmod 0640 $CONFDIR/$CONFHBAFILE
+chmod 0600 $PGPASS/$PGPASSFILE
+chmod 0600 $ROOTPATH/$PGPASSFILE
 
 echo "New config installed"
 
