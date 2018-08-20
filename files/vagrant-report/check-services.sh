4dcef6836fbe59fca25bf2881f0c21b30133a597
Niels Thomas Haugård <thomas@haugaard.net>
2018-08-20 14:50:02 +0200

    updates

--- files/vagrant-report/check-services.sh
+++ files/vagrant-report/check-services.sh
@@ -2,20 +2,39 @@
 
 # This script checks various services and reports back to user during provision.
 
-
-## Check for running services
-echo "----------------------"
-echo "Is PostgreSQL running?"
-/usr/sbin/service postgresql status |grep Active:
-echo
-
-echo "Is PostgreSQL configuration OK?"
+## Check services
+echo "Check PostgreSQL configuration OK"
+echo "---------------------------------"
 HBA_FILE=`sudo su postgres -c "psql -t -P format=unaligned -c 'show hba_file';"`
 if [ -f $HBA_FILE ]; then
     echo "psql running and reporting $HBA_FILE as config file ok"
 else
     echo "postgres installation failed"
 fi
+echo
+
+#  check reading from database via pgpool2
+echo "Check connection to database via pgpool2"
+echo "----------------------------------------"
+export PGPASSWORD="password"
+Q="select pg_is_in_recovery();"
+Q="SELECT
+ *\x
+FROM flow.fastnetmoninstances where vpn_ip_addr = '192.168.67.3';"
+
+LINES=`echo "${Q}" | psql -t -F' ' -h 127.0.0.1 -A -U flowuser -v ON_ERROR_STOP=1 -w -d netflow | wc -l`
+
+if [ $LINES -gt 100 ]; then
+    echo "read $LINES (expected 119) from database ok using pgpool2"
+else
+    echo "failed reading from database using pgpool2"
+fi
+echo
+
+## Check for running services
+echo "----------------------"
+echo "Is PostgreSQL running?"
+/usr/sbin/service postgresql status |grep Active:
 echo
 
 echo "---------------------"
