dfa768f6ac90655f43a189c205c8d394e721926e
Kasper Sort <kasper.sort@deic.dk>
2018-08-21 15:37:30 +0200

    Added config for port 80

--- files/nginx/env.sh
+++ files/nginx/env.sh
@@ -2,12 +2,17 @@ export TEST_TRY_FILES='$uri $uri/ =404'
 export X_ACS='https://$host$request_uri'
 
 
+export HOST='$host'
+export REQUEST_URI='$request_uri'
+
+export NGXPORT80='80'
 export NGXPORT8080='8080'
 export NGXPORT9090='9090'
 export LOCALPORT1='8686'
 export LOCALPORT2='9696'
 export LOCALHOST='127.0.0.1'
-export NGXSERVERNAME='ddps-demo'
+export NGXSERVERNAME='ddps.deic.dk'
+export NGXSERVERALIAS='www.ddps.deic.dk'
 export NGXROOT='/opt/ngx/ddosgui'
 export NGXINDEX='index.html index.htm'
 export TRY_FILES='$uri $uri/ /index.html?/$request_uri'
