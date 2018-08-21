93257656e3571cb4c509e90dae2621f7891e9261
Niels Thomas Haugård <thomas@haugaard.net>
2018-08-21 11:45:48 +0200

    changes to nginx installation

--- files/nginx/install.sh
+++ files/nginx/install.sh
@@ -22,4 +22,9 @@ fi
 
 apt-get -y install $PKG
 
+mkdir /etc/systemd/system/nginx.service.d
+printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
+systemctl daemon-reload
+systemctl restart nginx
+
 /bin/bash $VAGRANTPATH/$PKG/configure.sh
