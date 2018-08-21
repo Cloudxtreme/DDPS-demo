c7591ec4f966784f15f2184578b87a49a423e7e7
Niels Thomas Haugård <thomas@haugaard.net>
2018-08-21 11:50:55 +0200

    removed duplicte function in postgres configure.sh

--- files/postgresql/configure.sh
+++ files/postgresql/configure.sh
@@ -43,54 +43,6 @@ function savefile()
 	fi
 }
 
-function make_sftp_user()
-{
-	getent passwd ddpsadm > /dev/null 2>&1  >/dev/null || adduser --home /home/ddpsadm --shell /bin/bash --gecos "DDPS admin" --ingroup staff --disabled-password ddpsadm
-
-	if grep -q sftpgroup /etc/group
-    then
-         :
-    else
-		addgroup --system sftpgroup
-    fi
-
-	if [ -f /home/sftpgroup/newrules/.ssh/authorized_keys ]; then
-		chattr -i /home/sftpgroup/newrules/.ssh/authorized_keys /home/sftpgroup/newrules/.ssh/	>/dev/null 2>&1
-		rm -fr /home/sftpgroup/																	>/dev/null 2>&1
-		userdel -r newrules																		>/dev/null 2>&1
-	fi
-
-	mkdir /home/sftpgroup; chown root:root /home/sftpgroup
-
-	echo "$MYNAME: adduser 'newrules' sftp user for fastnetmon upload .... "
-	getent passwd newrules >/dev/null 2>&1 >/dev/null || useradd -m -c "DDPS rules upload" -d /home/sftpgroup/newrules/ -s /sbin/nologin newrules
-	usermod -G sftpgroup newrules
-    usermod -p '*'       newrules
-
-	chmod 755          /home/sftpgroup /home/sftpgroup/newrules/
-	mkdir -p           /home/sftpgroup/newrules/.ssh
-	chmod 700          /home/sftpgroup/newrules/.ssh
-	chown -R root:root /home/sftpgroup /home/sftpgroup/newrules/
-
-	test -d /home/sftpgroup/newrules/upload || mkdir /home/sftpgroup/newrules/upload
-	chown newrules:newrules /home/sftpgroup/newrules/upload
-	chmod 777 /home/sftpgroup/newrules/upload
-
-	echo "$MYNAME: permissions for /home/sftpgroup has been set carefully, dont change"
-	echo "$MYNAME: use chattr to lock / unlock /home/sftpgroup/newrules/.ssh/authorized_keys"
-
-	chattr -i /home/sftpgroup/newrules/.ssh/
-	if [ -f /home/sftpgroup/newrules/.ssh/authorized_keys ]; then
-		chattr -i /home/sftpgroup/newrules/.ssh/authorized_keys
-	fi
-	# this is a dummy key
-	cat << EOF | tr -d '\n' > /home/sftpgroup/newrules/.ssh/authorized_keys
-ssh-ed25519 AAAAC3NIamAdummyKeyJustToSeIfaScriptWorksAsExprecredXXXXXXXXXXXXXXXX root@00:25:90:46:c2:fe-fastnetmon2.deic.dk
-EOF
-	chown -R newrules:newrules /home/sftpgroup/newrules/.ssh
-	chattr +i /home/sftpgroup/newrules/.ssh   /home/sftpgroup/newrules/.ssh/*
-}
-
 function create_example_database()
 {
     # add data and schema to database
@@ -142,7 +94,6 @@ function main()
 
     . /vagrant/files/postgresql/vars.SH
 
-    make_sftp_user
     create_example_database
 
     rm -f $TMPFILE
